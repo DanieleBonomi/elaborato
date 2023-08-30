96f1e892673628c0eed0af6abbfe7d36b82228fd
DanieleBonomi <137277600+DanieleBonomi@users.noreply.github.com>
2023-08-30 18:32:00 +0200

    First tests created: I will refactor Server to avoid having to deal with a singleton while testing. It was bad design after all

---
+++ test/BaseFixture.cpp
@@ -0,0 +1,8 @@
+//
+// Created by user on 30/08/2023.
+//
+#include "gtest/gtest.h"
+
+class BaseFixture : public ::testing::Test {
+
+};
--- test/test.cpp
+++ test/test.cpp
@@ -3,9 +3,10 @@
 //
 
 #include "gtest/gtest.h"
+#include "../headers/Server.h"
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
+
     return RUN_ALL_TESTS();
 }
-
