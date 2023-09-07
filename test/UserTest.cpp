271d06827d2b283b68dfa6409acd087cbf69ede5
DanieleBonomi <137277600+DanieleBonomi@users.noreply.github.com>
2023-09-07 09:54:19 +0200

    Separated tests in UserTest.cpp

--- test/UserTest.cpp
+++ test/UserTest.cpp
@@ -8,7 +8,7 @@
 #include "../headers/User.h"
 #include "ServerFixture.cpp"
 
-TEST_F(ServerFixture,WriteInChats) { // should I divide this in more TEST_F? Probably
+TEST_F(ServerFixture,BasicWriteTest) {
 
 
     std::string str = "Hi Test";
@@ -18,15 +18,21 @@ TEST_F(ServerFixture,WriteInChats) { // should I divide this in more TEST_F? Pro
         server.getUserAtUsername(t)->writeMessage(str, 0);
         ASSERT_EQ(server.getMessageFromChat(0).size(), 1) << "MessageLog empty after first message";
         EXPECT_TRUE(server.getMessageFromChat(0).back()->getText() == str) << "First message was damaged in MessageLog";
-    } catch (std::exception& e) { ASSERT_TRUE(false) << e.what();}
+    } catch (std::exception &e) { ASSERT_TRUE(false) << e.what(); }
 
+}
+TEST_F(ServerFixture,UnicodeTest) {
 
     std::string str2 = "È interessante capire che succede con le accentate";
     try {
         server.getUserAtUsername(t)->writeMessage(str2, 0);
-    } catch (std::exception & e) {ASSERT_TRUE(false) << "Unicode messages cause error" << e.what(); }
+    } catch (std::exception &e) { ASSERT_TRUE(false) << "Unicode messages cause error" << e.what(); }
 
-    EXPECT_TRUE(server.getMessageFromChat(0).back()->getText()==str2 && server.getMessageFromChat(0).back()->getSender()==server.getUserAtUsername(t)) << "Message with unicode was damaged";
+    EXPECT_TRUE(server.getMessageFromChat(0).back()->getText() == str2 &&
+                server.getMessageFromChat(0).back()->getSender() == server.getUserAtUsername(t))
+                        << "Message with unicode was damaged";
+}
+TEST_F(ServerFixture,onMessageReceivedTest) {
 
     Server s2;
     s2.addChat(0);
@@ -36,4 +42,6 @@ TEST_F(ServerFixture,WriteInChats) { // should I divide this in more TEST_F? Pro
     m = s2.getUserAtUsername(t)->onMessageReceived(std::move(m));
     s2.onMessageReceived(std::move(m));
     EXPECT_TRUE(s2.getMessageFromChat(0).back()->getText()==str3 && s2.getMessageFromChat(0).back()->getSender()==s2.getUserAtUsername(t))<< "direct onMessageReceived not working";
+
+
 }
